0533bf412f370a8f6f590dabe16cf6812587dd54
spuithori <biz@spuit.tech>
2023-12-29 00:30:55 +0900

    Initial commit

---
+++ android/src/main/java/com/capacitor/dynamiccolor/DynamicColor.java
@@ -0,0 +1,11 @@
+package com.capacitor.dynamiccolor;
+
+import android.util.Log;
+
+public class DynamicColor {
+
+    public String echo(String value) {
+        Log.i("Echo", value);
+        return value;
+    }
+}
